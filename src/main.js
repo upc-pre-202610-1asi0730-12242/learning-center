2a9c1de7f503c24a2a21be31c1f286d75982802c
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-27 11:06:32 -0500

    chore: integrate prime-vue components and styles.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,49 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Material from '@primeuix/themes/material';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import {
+    Button,
+    Card,
+    Checkbox,
+    Column,
+    ConfirmationService,
+    ConfirmDialog, DataTable, Dialog,
+    DialogService, Drawer, FileUpload, FloatLabel, IconField, InputIcon, InputNumber, InputText, Menu,
+    Rating, Row, Select, SelectButton, Tag, Textarea, Toast,
+    ToastService, Toolbar, Tooltip
+} from "primevue";
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(PrimeVue, { theme: { preset: Material}, ripple: true})
+    .use(ConfirmationService)
+    .use(DialogService)
+    .use(ToastService)
+    .component('pv-button', Button)
+    .component('pv-card', Card)
+    .component('pv-column', Column)
+    .component('pv-confirm-dialog', ConfirmDialog)
+    .component('pv-checkbox', Checkbox)
+    .component('pv-data-table', DataTable)
+    .component('pv-dialog', Dialog)
+    .component('pv-drawer', Drawer)
+    .component('pv-select', Select)
+    .component('pv-select-button', SelectButton)
+    .component('pv-file-upload', FileUpload)
+    .component('pv-float-label', FloatLabel)
+    .component('pv-icon-field', IconField)
+    .component('pv-input-icon', InputIcon)
+    .component('pv-input-number', InputNumber)
+    .component('pv-input-text', InputText)
+    .component('pv-menu', Menu)
+    .component('pv-rating', Rating)
+    .component('pv-row', Row)
+    .component('pv-tag', Tag)
+    .component('pv-textarea', Textarea)
+    .component('pv-toolbar', Toolbar)
+    .component('pv-toast', Toast)
+    .directive('tooltip', Tooltip)
+    .mount('#app')
