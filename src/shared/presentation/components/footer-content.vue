95c28ddd6c085c58d5558d1a9a247ebd798cf117
Angel Velasquez <pcisavel@upc.edu.pe>
2026-05-03 00:11:00 -0500

    feat(authentication): implement iam store for user authentication and session management.

--- src/shared/presentation/components/footer-content.vue
+++ src/shared/presentation/components/footer-content.vue
@@ -7,7 +7,7 @@
 <template>
   <div class="grid bg-primary mt-4 p-2 align-content-start">
     <div class="col-12 ml-3 align-items-center justify-content-center">
-      <p>Copyright &copy; 2025. ACME Studios</p>
+      <p>Copyright &copy; 2026. ACME Studios</p>
     </div>
     <div  class="col-12 ml-3 mt-1 align-items-center justify-content-center">
       <p>
